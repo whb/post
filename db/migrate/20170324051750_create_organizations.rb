bd3b75bc86519fb8c3a5b737dc92e7416fea48ea
Haibo Wu <wuhaibo@xllg.com>
2017-03-24 13:25:35 +0800

    Add bootstrap and Orgnization scaffold

---
+++ db/migrate/20170324051750_create_organizations.rb
@@ -0,0 +1,10 @@
+class CreateOrganizations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :organizations do |t|
+      t.string :name
+      t.integer :superior_id
+
+      t.timestamps
+    end
+  end
+end
