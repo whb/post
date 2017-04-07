791396a0d77914bc380e21cfae869ae909842b80
WuHaibo <haibowu@hotmail.com>
2017-04-07 14:59:25 +0800

    add share config for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :user, 'deploy'          # Username in the server to SSH to.
 
 # shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # set :shared_dirs, fetch(:shared_dirs, []).push('somedir')
-# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
