92fc51c789ee18806d3436de1b57e0ed6e5c40bb
Wu Haibo <haibowu@hotmail.com>
2017-08-24 15:38:51 +0800

    create update and delete fee

--- app/controllers/fees_controller.rb
+++ app/controllers/fees_controller.rb
@@ -18,11 +18,12 @@ class FeesController < ApplicationController
     @fee.begin_date = params[:begin_date] if params[:begin_date]
     @fee.end_date = params[:end_date] if params[:end_date]
 
-    @incomes = Income.where(:bill_date => (@fee.begin_date..@fee.end_date))
+    find_selected_incomes
   end
 
   # GET /fees/1/edit
   def edit
+    find_selected_incomes
   end
 
   # POST /fees
@@ -32,7 +33,7 @@ class FeesController < ApplicationController
     respond_to do |format|
       if @fee.save
         updateIncomes
-        format.html { redirect_to @fee, notice: 'Fee was successfully created.' }
+        format.html { redirect_to @fee, notice: t('Fee was successfully created.') }
         format.json { render :show, status: :created, location: @fee }
       else
         format.html { render :new }
@@ -46,7 +47,8 @@ class FeesController < ApplicationController
   def update
     respond_to do |format|
       if @fee.update(fee_params)
-        format.html { redirect_to @fee, notice: 'Fee was successfully updated.' }
+        updateIncomes
+        format.html { redirect_to @fee, notice: t('Fee was successfully updated.') }
         format.json { render :show, status: :ok, location: @fee }
       else
         format.html { render :edit }
@@ -59,8 +61,9 @@ class FeesController < ApplicationController
   # DELETE /fees/1.json
   def destroy
     @fee.destroy
+    updateIncomes
     respond_to do |format|
-      format.html { redirect_to fees_url, notice: 'Fee was successfully destroyed.' }
+      format.html { redirect_to fees_url, notice: t('Fee was successfully destroyed.') }
       format.json { head :no_content }
     end
   end
@@ -71,13 +74,27 @@ class FeesController < ApplicationController
       @fee = Fee.find(params[:id])
     end
 
+    def find_selected_incomes
+      @incomes = (Income.where(bill_date: (@fee.begin_date..@fee.end_date), fee: nil)).or(Income.where(fee: @fee))
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def fee_params
       params.require(:fee).permit(:begin_date, :end_date, :income_amount, :fee_amount, :part_amount1, :percent1, :part_amount2, :percent2)
     end
 
     def updateIncomes
+      @fee.incomes.each do |income|
+        income.fee = nil
+        income.fee_amount = nil
+        income.save
+      end
+
+      return if params == nil || params[:fee] == nil
+
       params[:fee][:incomes_attributes].each do |key, incomeParam|
+        next if(incomeParam[:id] == nil)
+
         income = Income.find(incomeParam[:id])
         income.fee = @fee
         income.fee_amount = incomeParam[:fee_amount]
--- app/helpers/fees_helper.rb
+++ app/helpers/fees_helper.rb
@@ -1,2 +1,10 @@
 module FeesHelper
+	def income_id_checkbox(i, income_id, fee_incomes)
+		checked_flag = fee_incomes.map{|income| income.id}.include?(income_id) ? "checked='checked'" : ""
+		return "<input class='fee_incomes_id' name='fee[incomes_attributes][#{i}][id]' type='checkbox' value='#{income_id}' #{checked_flag} />".html_safe
+	end		
+
+	def income_fee_amount_input(i, income)
+		return "<input class='fee_incomes_fee_amount numeric decimal optional' name='fee[incomes_attributes][#{i}][fee_amount]' type='number' value='#{income.fee_amount}'/>".html_safe
+	end	
 end
